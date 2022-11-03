543d78dad6da533f080837ac880c96ac83ff5c36
ovie-daveth <105714886+ovie-daveth@users.noreply.github.com>
2022-11-03 23:21:57 +0100

    3rd day of challenge

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+const navlist = document.querySelector(".nav-list-1");
+
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    navlist.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    menu.classList.remove("active");
+    navlist.classList.remove("active");
+}))
